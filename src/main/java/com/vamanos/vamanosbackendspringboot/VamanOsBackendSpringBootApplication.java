3b31ca4abdb9c1141a84f9c4471fec0036139db8
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2019-08-18 08:50:34 +0530

    Initial Commit

---
+++ src/main/java/com/vamanos/vamanosbackendspringboot/VamanOsBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vamanos.vamanosbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VamanOsBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VamanOsBackendSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vamanos/vamanosbackendspringboot/VamanOsBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamanos.vamanosbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VamanOsBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
